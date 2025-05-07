7d3d24668fbd6d345ee563a3a707e2685dfef8d4
JamesKRoddy <47905049+JamesKRoddy@users.noreply.github.com>
2025-05-07 22:19:29 +0100

    Update RobotCharacterController.cs

--- Assets/Scripts/Characters/NPCs/RobotCharacterController.cs
+++ Assets/Scripts/Characters/NPCs/RobotCharacterController.cs
@@ -96,6 +96,10 @@ public class RobotCharacterController : HumanCharacterController
             currentWorkTask.StopWorkCoroutine();
             currentWorkTask = null;
             animator.Play("Empty", workLayerIndex);
+
+            if(PlayerInput.Instance.CurrentControlType != PlayerControlType.IN_MENU){
+                PlayerInput.Instance.UpdatePlayerControls(PlayerControlType.ROBOT_MOVEMENT);
+            }
         }
     }
 
