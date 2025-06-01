d19373522e644aef91db5a6e53f8fb79e08c5541
JamesKRoddy <jameskr024@gmail.com>
2025-06-01 23:07:38 +0100

    Damage over time

--- Assets/Scripts/Enviroment/DamageArea.cs
+++ Assets/Scripts/Enviroment/DamageArea.cs
@@ -6,19 +6,30 @@ using System.Collections;
 public class DamageArea : MonoBehaviour
 {
     private float damage;
+    private float damageInterval = 1f; // Time between damage applications
+    private float lastDamageTime; // When we last applied damage
 
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.GetComponent<IDamageable>() != null)
         {
-            // Damage the player if they are in the vomit pool
-            other.gameObject.GetComponent<IDamageable>().TakeDamage(damage);
+            // Only apply damage if enough time has passed since last damage
+            if (Time.time >= lastDamageTime + damageInterval)
+            {
+                other.gameObject.GetComponent<IDamageable>().TakeDamage(damage);
+                lastDamageTime = Time.time;
+            }
         }
     }
 
     public void SetDamage(float dmg)
     {
         damage = dmg;
+    }
+
+    public void SetDamageInterval(float interval)
+    {
+        damageInterval = interval;
     }
 }
 
