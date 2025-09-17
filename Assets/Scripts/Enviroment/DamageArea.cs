6564490884b4d3753a1bcb883b0cff53993613fc
JamesKRoddy <jameskr024@gmail.com>
2025-09-17 21:09:39 +0100

    Update DamageArea.cs

--- Assets/Scripts/Enviroment/DamageArea.cs
+++ Assets/Scripts/Enviroment/DamageArea.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Rigidbody))]
@@ -7,17 +8,24 @@ public class DamageArea : MonoBehaviour
 {
     private float damage;
     private float damageInterval = 1f; // Time between damage applications
-    private float lastDamageTime; // When we last applied damage
+    private Dictionary<IDamageable, float> lastDamageTimes = new Dictionary<IDamageable, float>(); // Track damage times per IDamageable
 
     void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.GetComponent<IDamageable>() != null)
+        IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
+        if (damageable != null)
         {
-            // Only apply damage if enough time has passed since last damage
-            if (Time.time >= lastDamageTime + damageInterval)
+            // Check if we have a record for this damageable
+            if (!lastDamageTimes.ContainsKey(damageable))
             {
-                other.gameObject.GetComponent<IDamageable>().TakeDamage(damage);
-                lastDamageTime = Time.time;
+                lastDamageTimes[damageable] = 0f; // Initialize with 0 so it can take damage immediately
+            }
+            
+            // Only apply damage if enough time has passed since last damage for this specific damageable
+            if (Time.time >= lastDamageTimes[damageable] + damageInterval)
+            {
+                damageable.TakeDamage(damage);
+                lastDamageTimes[damageable] = Time.time;
             }
         }
     }
@@ -30,6 +38,16 @@ public class DamageArea : MonoBehaviour
     public void SetDamageInterval(float interval)
     {
         damageInterval = interval;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
+        if (damageable != null && lastDamageTimes.ContainsKey(damageable))
+        {
+            // Remove the damageable from our tracking when it leaves the trigger
+            lastDamageTimes.Remove(damageable);
+        }
     }
 }
 
